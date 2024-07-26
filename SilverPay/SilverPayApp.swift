7b76ebdb41312c9128b039ece56738b689e6b6eb
Ayush Bhople <ayush.bhople@innovapptive.com>
2024-07-26 12:26:09 +0530

    Pilot

---
+++ SilverPay/SilverPayApp.swift
@@ -0,0 +1,17 @@
+//
+//  SilverPayApp.swift
+//  SilverPay
+//
+//  Created by E5000848 on 26/07/24.
+//
+
+import SwiftUI
+
+@main
+struct SilverPayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
